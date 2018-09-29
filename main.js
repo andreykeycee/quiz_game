e724ccc68a0f2f4bd0870f0bbc4458c9c6c603d4
andreykeycee <tessarianisbest@gmail.com>
2018-09-29 19:46:36 +0300

    Strategies setted but check answers still doesnt work

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 window.onload = function() {
+  //options
+  const optionOl = document.getElementById('options');
+  // question head
+  const questH = document.getElementById('question');
   // divs
   const greetDiv = document.getElementById('greetings');
   const quizDiv = document.getElementById('quiz');
@@ -7,8 +11,7 @@ window.onload = function() {
   const goButton = document.getElementById('go');
   const confButton = document.getElementById('confirm');
   const submButton = document.getElementById('submit');
-  // question head
-  const questH = document.getElementById('question');
+
   // result list
   const resultList = document.getElementById('results');
   // hadler arrays
@@ -16,10 +19,30 @@ window.onload = function() {
   // declare count for questions array
   let count = 0;
 
-  // define function which fill head and radio buttons, + confirm, and give you a 10 sec to decide
+  goButton.addEventListener('click', function() {
+    greetDiv.className = 'hide';
+    quizDiv.className = '';
+    loop();
+  })
+
+  confButton.addEventListener('click', function() {
+    questions[count].check(questions[count].choises, questions[count].correct, answers);
+    count++;
+    console.log(answers);
+    if(count < questions.length) {
+      loop();
+    } else {
+      quizDiv.className = 'hide';
+      resultDiv.className = '';
+    }
+  })
+
+  // // define function which fill head and radio buttons, + confirm, and give you a 10 sec to decide
   function loop() {
-    // fill head
+    optionOl.innerHTML = '';
     questH.innerHTML = questions[count].quest;
+    questions[count].render(questions[count].choises, optionOl);
+
     setTimeout(function() {
       count++;
       if(count < questions.length) {
@@ -28,50 +51,4 @@ window.onload = function() {
       }
     }, 10000)
   }
-
-  function showResult() {
-    // quizDiv.className = 'hide';
-    // resultDiv.className = '';
-    // for(let i = 0; i < answers.length; i++) {
-    //   const resultLi = document.createElement('li');
-    //   if(answers[i] == questions[i].correct) {
-    //     resultLi.textContent = answers[i] + ' correct!';
-    //   } else {
-    //     resultLi.textContent = answers[i] + ' wrong!';
-    //   }
-    //   resultList.appendChild(resultLi);
-    // }
-  }
-
-  // add event listener on go button
-  goButton.addEventListener('click', function() {
-    greetDiv.className = 'hide';
-    quizDiv.className = '';
-    loop();
-  });
-
-  // add event listener on confirm button
-  // confButton.addEventListener('click', function() {
-  //   // loop through the radios to find checked one
-  //   for(let i = 0; i < options.length; i++) {
-  //     // if checked, push answers to answers array
-  //     // if we have a qustions before use loop function again,
-  //     // if else
-  //     if(options[i].checked === true) {
-  //       answers.push(optSpans[i].textContent);
-  //       count++;
-  //       console.log(answers);
-  //       if(count < questions.length) {
-  //         loop();
-  //       } else {
-  //         showResult();
-  //       }
-  //     }
-  //   }
-  // })
-
-  submButton.addEventListener('click', function() {
-    resultDiv.className = 'hide';
-    greetDiv.className = '';
-  })
 }
--- questions.js
+++ questions.js
@@ -1,36 +1,95 @@
-window.onload = function() {
 
-  // options
-  const optionOl = document.getElementById('options');
+// constructor question
+const Question = function(quest, strategy, choises, correct) {
+  this.quest = quest;
+  this.strategy = strategy;
+  this.choises = choises;
+  this.correct = correct;
+};
 
-  const Question = function(quest, optionsChoise, correct) {
-    this.quest = quest;
-    this.optionsChoise = optionsChoise;
-    this.correct = correct
+Question.prototype = {
+  render: function(choises, ol) {
+    return this.strategy.render(choises, ol);
+  },
+  check: function(choises, correct, answers) {
+    return this.strategy.check(choises, correct, answers);
   }
-  // make a prototype of strategies for Quesion
-  Question.prototype.choise = function() {
-    return this.optionsChoise();
-  }
-  // define strategies
-  const radioStrategy = function(radioChoises) {
-    for(let i = 0; i < radioChoises.length; i++) {
-      const radio = document.createElement('input');
+}
+// define strategy constructor
+const Strategy = function() {};
+// set strategy prototype
+Strategy.prototype = {
+  render: function() {},
+  check: function() {}
+};
+// define strategies
+const radioStrategy = function() {};
+const checkboxStrategy = function() {};
+// copy strategy properties from parent
+radioStrategy.prototype = Object.create(Strategy.prototype);
+checkboxStrategy.prototype = Object.create(Strategy.prototype);
+// define radio strategy prototype
+radioStrategy.prototype = {
+  render: function(choises, ol) {
+    for(let i = 0; i < choises.length; i++) {
       const li = document.createElement('li');
+      const radio = document.createElement('input');
       radio.type = 'radio';
-      radio.name = 'choises';
-      li.textContent = radioChoises[i];
+      radio.name = 'radio';
+      li.textContent = choises[i];
       li.appendChild(radio);
-      optionOl.appendChild(li);
+      ol.appendChild(li);
+    }
+  },
+  check: function(choises, correct, answers) {
+    let answer;
+    for(let i = 0; i < choises.length; i++) {
+      if(choises[i].checked === true) {
+        answer = choises[i];
+      }
+    }
+    console.log(answer);
+    if(answer == correct) {
+      return answers.push(true);
+    } else {
+      return answers.push(false);
     }
   }
+}
+// define checkbox strategy render
+checkboxStrategy.prototype = {
+  render: function(choises, ol) {
+    for(let i = 0; i < choises.length; i++) {
+      const li = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.name = 'checkbox';
+      li.textContent = choises[i];
+      li.appendChild(checkbox);
+      ol.appendChild(li);
+    }
+  },
+  check: function(choises, correct, answers) {
+    let answer = [];
+    for(let i = 0; i < choises.length; i++) {
+      if(choises[i].checked === true) {
+        answer.push(choises[i]);
+      }
+    }
 
-  // const radioBlock = new optionsChoise(radioStrategy);
+    if(answer == correct) {
+      return answers.push(true);
+    } else {
+      return answers.push(false);
+    }
+  }
+}
 
+const Q1 = new Question('How many choises here?', new radioStrategy(), [2, 3, 4, 5], 4),
+      Q2 = new Question(
+        'Check things, which true', new checkboxStrategy(),
+        ['white div', 'black sun', 'confirm button', 'you are an alien'],
+        ['white div', 'confirm button']
+      );
 
-  const Q1 = new Question('How many choises here?', radioStrategy([2, 3, 4, 5]), 4);
-        // Q2 = new Question('How many legs human have?', [2, 3, 4, 5], 2),
-        // Q3 = new Question('How many legs spiders have?', [3, 8, 4, 5], 8);
-
-  const questions = [Q1];
-};
+const questions = [Q1, Q2];
